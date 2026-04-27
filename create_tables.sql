2d8165582e317de4d2c6cf70b6f6692a4ce5507f
anothergrind <ozorjikj@gmail.com>
2026-04-27 15:48:31 -0500

    created following tables: phone number, member, employee, librarycard, silver, and gold

--- create_tables.sql
+++ create_tables.sql
@@ -17,3 +17,119 @@ CREATE TABLE PERSON (
     CONSTRAINT pk_person PRIMARY KEY (PersonID),
     CONSTRAINT chk_personid CHECK (PersonID LIKE 'P[0-9][0-9][0-9]')
 );
+
+CREATE TABLE PHONENUMBER(
+    PhoneNumber CHAR(22)    NOT NULL,
+    PersonID    CHAR(4)     NOT NULL,
+    PhoneType   VARCHAR(10)
+
+    CONSTRAINT pk_phonenumber PRIMARY KEY (PhoneNumber),
+    CONSTRAINT fk_phonenumber_person FOREIGN KEY (PersonID)
+        REFERENCES PERSON(PersonID)
+        ON DELETE CASCADE --might delete, not sure if we talked about this in class
+);
+
+CREATE TABLE MEMBER(
+    PersonID    CHAR(4)     NOT NULL
+
+    CONSTRAINT fk_phonenumber_person FOREIGN KEY (PersonID)
+        REFERENCES PERSON(PersonID)
+        ON DELETE CASCADE --might delete, not sure if we talked about this in class
+);
+
+CREATE TABLE EMPLOYEE(
+    PersonID    CHAR(4)     NOT NULL,
+    StartDate   DATE        NOT NULL
+
+    CONSTRAINT fk_phonenumber_person FOREIGN KEY(PersonID)
+        REFERENCES PERSON(PersonID)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE LIBRARY_CARD(
+    CardID              CHAR(10)    NOT NULL,
+    PersonID            CHAR(4)     NOT NULL,
+    MembershipLevel     VARCHAR(10)
+    IssueDate           DATE        NOT NULL
+
+    CONSTRAINT pk_card PRIMARY KEY (CardID),
+    CONSTRAINT fk_phonenumber_person FOREIGN KEY(PersonID)
+        REFERENCES PERSON(PersonID)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE SILVER(
+    CardID  CHAR(10)    NOT NULL
+
+    CONSTRAINT fk_card FOREIGN KEY (CardID)
+        REFERENCES CARD(CardID)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE GOLD(
+    CardID  CHAR(10)    NOT NULL
+
+    CONSTRAINT fk_card FOREIGN KEY (CardID)
+        REFERENCES CARD(CardID)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE GUESTLOG(
+
+);
+
+CREATE TABLE PROMOTION(
+
+);
+
+CREATE TABLE CARD_PROMOTION(
+
+);
+
+CREATE TABLE SUPERVISOR(
+
+);
+
+CREATE TABLE CATALOGING_MANAGER(
+
+);
+
+CREATE TABLE LIBRARY_RECEPTIONIST(
+
+);
+
+CREATE TABLE PUBLISHER(
+
+);
+
+CREATE TABLE BOOK(
+
+);
+
+CREATE TABLE AUTHOR(
+
+);
+
+CREATE TABLE BOOK_AUTHOR(
+
+);
+
+CREATE TABLE PAYMENT(
+
+);
+
+CREATE TABLE BORROWING(
+
+);
+
+CREATE TABLE INQUIRY(
+
+);
+
+CREATE TABLE COMMENT(
+
+);
+
+CREATE TABLE CATALOGING_RECORD(
+
+);
