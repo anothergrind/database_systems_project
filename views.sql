d705bf60e6a78ddbb7313903839c667160fad59b
anothergrind <ozorjikj@gmail.com>
2026-04-27 23:04:56 -0500

    added created table views

--- views.sql
+++ views.sql
@@ -1,8 +1,55 @@
--- 4. Use the Create View statement to create the following views:
+-- JUNGLE LIBRARY DATABASE
+-- PHASE III: Create Views (Q4)
 
--- TODOS:
--- [1] TopGoldMember - This view returns the First Name, Last Name, and Date of membership enrollment of those members who have borrowed more than 6 books in the past year.
--- [2] PopularBooks - This view returns the details of the top 10 most borrowed books over the past year.
--- [3] BestRatingPublisher - This view returns the names of publishers with more than three books whose average rating scores are at least 4.5
--- [4] PotentialGoldMember - This view returns the name, phone number, and ID of silver members who have borrowed books more than two times in the past year.
--- [5] ActiveReceptionist - This view returns the names of receptionists who resolved more than 10 inquiries from members in the past month.
+-- 1. TopGoldMember
+CREATE VIEW TopGoldMember AS
+SELECT P.FName, P.LName, LC.IssueDate
+FROM PERSON P
+JOIN LIBRARY_CARD LC ON P.PersonID = LC.PersonID
+JOIN GOLD G          ON LC.CardID = G.CardID
+JOIN BORROWING B     ON P.PersonID = B.PersonID
+WHERE B.IssueDate >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR)
+GROUP BY P.PersonID, P.FName, P.LName, LC.IssueDate
+HAVING COUNT(B.BorrowID) > 6;
+
+-- 2. PopularBooks
+CREATE VIEW PopularBooks AS
+SELECT Bo.BookID, Bo.PublisherID, Bo.Title, Bo.Category, Bo.ISBN, Bo.Edition, COUNT(Bw.BorrowID) AS BorrowCount
+FROM BOOK Bo
+JOIN BORROWING Bw ON Bw.BookID = Bo.BookID
+WHERE Bw.IssueDate >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR)
+GROUP BY Bo.BookID, Bo.PublisherID, Bo.Title, Bo.Category, Bo.ISBN, Bo.Edition
+ORDER BY BorrowCount DESC
+LIMIT 10;
+
+-- 3. BestRatingPublisher
+CREATE VIEW BestRatingPublisher AS
+SELECT P.PublisherName
+FROM PUBLISHER P
+JOIN BOOK B    ON B.PublisherID = P.PublisherID
+JOIN COMMENT C ON C.BookID = B.BookID
+GROUP BY P.PublisherID, P.PublisherName
+HAVING COUNT(DISTINCT B.BookID) > 3 AND AVG(C.RatingScore) >= 4.5;
+
+-- 4. PotentialGoldMember
+CREATE VIEW PotentialGoldMember AS
+SELECT P.FName, P.LName, P.PersonID,
+       (SELECT PN.PhoneNumber FROM PHONENUMBER PN
+            WHERE PN.PersonID = P.PersonID LIMIT 1) AS PhoneNumber
+FROM PERSON P
+JOIN LIBRARY_CARD LC ON P.PersonID = LC.PersonID
+JOIN SILVER S        ON LC.CardID = S.CardID
+JOIN BORROWING B     ON P.PersonID = B.PersonID
+WHERE B.IssueDate >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR)
+GROUP BY P.PersonID, P.FName, P.LName
+HAVING COUNT(B.BorrowID) > 2;
+
+-- 5. ActiveReceptionist
+CREATE VIEW ActiveReceptionist AS
+SELECT P.FName, P.LName
+FROM PERSON P
+JOIN LIBRARY_RECEPTIONIST LR ON P.PersonID = LR.PersonID
+JOIN INQUIRY I               ON LR.PersonID = I.ReceptionistID
+WHERE I.ResolutionStatus = 'Resolved' AND I.InquiryTime >= DATE_SUB(NOW(), INTERVAL 1 MONTH)
+GROUP BY P.PersonID, P.FName, P.LName
+HAVING COUNT(I.InquiryID) > 10;
